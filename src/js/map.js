9e4bcc2c9e82a0283c1945d20bed2d035b9cfdbb
Joseph <madhatters@gmail.com>
2016-11-06 09:15:43 -0800

    Resubmit

--- src/js/map.js
+++ src/js/map.js
@@ -2,6 +2,8 @@
  * Created by Joseph on 8/30/2016.
  */
 var map;
+var infoWindow;
+var bounds;
 
 function initMap(){
     var geoCords = [];
@@ -9,12 +11,22 @@ function initMap(){
         center: {lat: 37.414653, lng: -122.078533},
         zoom: 15
     });
+    infoWindow = new google.maps.InfoWindow();
+    ko.applyBindings(new LocationViewModel());
+}
+
+function mapError() {
+    alert('Google Maps failed to load, refresh page');
 }
 
 function resizeMap(markers) {
-    var bounds = new google.maps.LatLngBounds();
+    bounds = new google.maps.LatLngBounds();
     for(var i = 0; i < markers.length; i++) {
         bounds.extend(markers[i].getPosition());
     }
-    map.fitBounds(bounds);
+
 }
+
+window.onresize = function() {
+    map.fitBounds(bounds);
+};
--- src/js/viewModel.js
+++ src/js/viewModel.js
@@ -4,13 +4,37 @@
 var MAP_KEY = 'AIzaSyCkW6FCwPhwzhiHG48DbjzCFP_1lGXLQWA';
 var CLIENT_ID = 'ZWI00VLLOOCDO3KNPL1URSOBTSK3BNUVJYTI3PDMDQES4E2S';
 var CLIENT_SECRECT = 'QLCIS5IGUN1QWOW5GLCKBTAKIISEFE3QHTS011R1C0DLI1DP';
-var infoWindow = new google.maps.InfoWindow();
+
 
 var headerHTML = '<h1 class="info-header">%%data</h1>';
 var infoHTML = '<p class="info-p">%%data %%data</p>';
 var hyperLinkHTML = '<a href="%%data" class="info-hyperlink">%%data</a>';
 var imageHTML = '<img src="%%data">';
-var filterInputElement = $('#filter-input');
+
+function Errors() {
+    var self = this;
+    self.fourSquareErrorCalled = false;
+    self.googleErrorCalled = false;
+    self.fourSquareError = function (error) {
+        if (self.fourSquareErrorCalled === false) {
+            self.errorPrompt(error);
+            self.fourSquareErrorCalled = true;
+        }
+    };
+
+    self.googleError = function (error) {
+        if (self.googleErrorCalled === false) {
+            self.errorPrompt(error);
+            self.googleErrorCalled = true;
+        }
+    };
+
+    self.errorPrompt = function (error) {
+        alert(error);
+    };
+}
+
+var errorNotification = new Errors();
 
 function Location(name, address) {
     var self = this;
@@ -21,65 +45,64 @@ function Location(name, address) {
     self.thirdPartyData = null;
 }
 
-function ListItem(item, visible) {
-    this.item = item;
-    this.visible = ko.observable(visible);
-}
-
 function LocationViewModel() {
 
     var self = this;
     var currentMarker = null;
-    self.listItems = ko.observableArray([
-        new ListItem(new Location("Google", "1600 Amphitheatre Pkwy, Mountain View, CA 94043"), true),
-        new ListItem(new Location("Shoreline Amphitheatre", "1 Amphitheatre Pkwy, Mountain View, CA 94043"), true),
-        new ListItem(new Location("Mozart Foundation Automobile Museum", "1325 Pear Ave, Mountain View, CA 94043"), true),
-        new ListItem(new Location("Computer History Museum", "1401 N Shoreline Blvd, Mountain View, CA 94043"), true),
-        new ListItem(new Location("Century Cinema 16", "1500 N Shoreline Blvd, Mountain View, CA 94043"), true)
+    self.filterInput = ko.observable('');
+    self.locationList = ko.observableArray([
+        new Location("Google", "1600 Amphitheatre Pkwy, Mountain View, CA 94043"),
+        new Location("Shoreline Amphitheatre", "1 Amphitheatre Pkwy, Mountain View, CA 94043"),
+        new Location("Mozart Foundation Automobile Museum", "1325 Pear Ave, Mountain View, CA 94043"),
+        new Location("Computer History Museum", "1401 N Shoreline Blvd, Mountain View, CA 94043"),
+        new Location("Century Cinema 16", "1500 N Shoreline Blvd, Mountain View, CA 94043")
 
     ]);
 
-    //Filters Locations by whatever is located in the filter input
-    self.filter = function () {
-        var filterBy = filterInputElement.val();
-        for (var i = 0; i < self.listItems().length; i++) {
-            if (self.listItems()[i].item.name.substring(0, filterBy.length).toLowerCase() === filterBy.toLowerCase()) {
-                self.listItems()[i].visible(true);
-                self.listItems()[i].item.marker.setMap(map);
-            }
-            else {
-                self.listItems()[i].visible(false);
-                self.listItems()[i].item.marker.setMap(null);
-            }
-        }
-        //If nothing is in the filter input remove filter
-        if (filterBy.length === 0) {
-            self.removeFilter();
-        }
-        return true;
-    };
-
     //Makes all list items visible and places all map markers back on map
     self.removeFilter = function () {
-        for (var i = 0; i < self.listItems().length; i++) {
-            self.listItems()[i].visible(true);
-            self.listItems()[i].item.marker.setMap(map);
+        for (var i = 0; i < self.locationList().length; i++) {
+            if (self.locationList()[i].marker !== null) {
+                self.locationList()[i].marker.setVisible(true);
+            }
         }
-        filterInputElement.val('');
+        self.filterInput('');
         infoWindow.close();
     };
+
+    //Filters Locations by whatever is located in the filter input
+    self.filteredLocations = ko.computed(function () {
+        //If nothing is in the filter input remove filter
+        if (!self.filterInput()) {
+            self.removeFilter();
+            return self.locationList();
+        }
+        else {
+            var filtered = ko.utils.arrayFilter(self.locationList(), function (location) {
+                return location.name.toLowerCase().indexOf(self.filterInput().toLowerCase()) !== -1;
+            });
+            for (var index = 0; index < self.locationList().length; index++) {
+                self.locationList()[index].marker.setVisible(false);
+            }
+            for (var i = 0; i < filtered.length; i++) {
+                filtered[i].marker.setVisible(true);
+            }
+            return filtered;
+        }
+    });
+
 
     //Gets Markers amd street view image from Google
     self.getMarker = function (name, address, expectedNum) {
         $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address=" + address.replace(/ /g, '+') + MAP_KEY, function (data) {
-            var index = findItemInArray(self.listItems, name);
-            self.listItems()[index].item.marker = new google.maps.Marker({
+            var index = findItemInArray(self.locationList, name);
+            self.locationList()[index].marker = new google.maps.Marker({
                 position: {lat: data.results[0].geometry.location.lat, lng: data.results[0].geometry.location.lng},
                 map: map,
                 title: name
             });
-            self.attachInfoWindow(self.listItems()[index].item.marker);
-            self.listItems()[index].item.thirdPartyPhotos = imageHTML.replace('%%data', 'https://maps.googleapis.com/maps/api/streetview?size=300x150' +
+            self.attachInfoWindow(self.locationList()[index].marker);
+            self.locationList()[index].thirdPartyPhotos = imageHTML.replace('%%data', 'https://maps.googleapis.com/maps/api/streetview?size=300x150' +
             '&location=' + data.results[0].geometry.location.lat + ',' + data.results[0].geometry.location.lng +
             '&heading=200&pitch=-0.76' +
             '&key=' + MAP_KEY);
@@ -87,13 +110,13 @@ function LocationViewModel() {
             numResponse++;
             if (numResponse === expectedNum) {
                 var markers = new Array(5);
-                for (var i = 0; i < self.listItems().length; i++) {
-                    markers[i] = self.listItems()[i].item.marker;
+                for (var i = 0; i < self.locationList().length; i++) {
+                    markers[i] = self.locationList()[i].marker;
                 }
                 resizeMap(markers);
             }
         }).fail(function (jqXHR, status, error) {
-            self.errorPrompt('Failed to load data from Google please refresh');
+            errorNotification.googleError('Failed to load data from Google please refresh');
         });
 
     };
@@ -112,24 +135,39 @@ function LocationViewModel() {
         }).done(function (response) {
             //Places thirdparty data in location and formats using html
             console.log(response);
-            var index = findItemInArray(self.listItems, name);
-            var info = headerHTML.replace('%%data', response.response.venues[0].name);
-            info = info + infoHTML.replace('%%data', 'People here now').replace('%%data', response.response.venues[0].hereNow.count);
-            info = info + infoHTML.replace('%%data', 'Check in count').replace('%%data', response.response.venues[0].stats.checkinsCount);
+            var index = findItemInArray(self.locationList, name);
+            var info;
+            if (response.response.venues[0].name !== undefined) {
+                info = headerHTML.replace('%%data', response.response.venues[0].name);
+            }
+            else {
+                info = headerHTML.replace('%%data', 'Name Could Not Loaded');
+            }
+            if (response.response.venues[0].hereNow.count !== undefined) {
+                info = info + infoHTML.replace('%%data', 'People here now').replace('%%data', response.response.venues[0].hereNow.count);
+            }
+            else {
+                info = info + infoHTML.replace('%%data', 'Could not load number of people here');
+            }
+            if (response.response.venues[0].stats.checkinsCount !== undefined) {
+                info = info + infoHTML.replace('%%data', 'Check in count').replace('%%data', response.response.venues[0].stats.checkinsCount);
+            }
+            else {
+                info = info + infoHTML.replace('%%data', 'Could not load number of checkins');
+            }
             info = info + hyperLinkHTML.replace('%%data', 'https://foursquare.com/').replace('%%data', 'Provided by Foursquare');
-            self.listItems()[index].item.thirdPartyData = info;
+            self.locationList()[index].thirdPartyData = info;
         }).fail(function (jqXHR, status, error) {
-            self.errorPrompt('Failed to load data from FourSquare please refresh');
+            errorNotification.fourSquareError('Failed to load data from FourSquare please refresh');
         });
     };
 
     self.attachInfoWindow = function (marker) {
         marker.addListener('click', function () {
-
             //find the list item that the marker belongs to
-            for (var i = 0; i < self.listItems().length; i++) {
-                if (marker === self.listItems()[i].item.marker) {
-                    self.displayInfoWindow(self.listItems()[i].item);
+            for (var i = 0; i < self.locationList().length; i++) {
+                if (marker === self.locationList()[i].marker) {
+                    self.displayInfoWindow(self.locationList()[i]);
                     break;
                 }
             }
@@ -137,26 +175,27 @@ function LocationViewModel() {
     };
 
     self.selectMarker = function (element) {
-        self.displayInfoWindow(element.item);
+        self.displayInfoWindow(element);
     };
 
-    self.displayInfoWindow = function (item) {
+    self.displayInfoWindow = function (location) {
         //If the item that is clicked is the same item that is currently selected reset the animation and close the info window
-        if (currentMarker === item.marker) {
+        if (currentMarker === location.marker) {
             self.resetMarkers();
         }
         else {
-            currentMarker = item.marker;
+            currentMarker = location.marker;
             self.resetOtherMarkers();
-            infoWindow.setContent(item.thirdPartyPhotos + item.thirdPartyData);
-            infoWindow.open(map, item.marker);
-            item.marker.setAnimation(google.maps.Animation.BOUNCE);
+            infoWindow.setContent(location.thirdPartyPhotos + location.thirdPartyData);
+            infoWindow.open(map, location.marker);
+            map.setCenter(currentMarker.getPosition());
+            location.marker.setAnimation(google.maps.Animation.BOUNCE);
         }
     };
 
     self.resetMarkers = function () {
-        for (var i = 0; i < self.listItems().length; i++) {
-            self.listItems()[i].item.marker.setAnimation(null);
+        for (var i = 0; i < self.locationList().length; i++) {
+            self.locationList()[i].marker.setAnimation(null);
         }
         infoWindow.close();
         currentMarker = null;
@@ -164,30 +203,27 @@ function LocationViewModel() {
 
     //Removes the animation from all markers so when the user clicks from one marker to another the previous one stops bouncing
     self.resetOtherMarkers = function () {
-        for (var i = 0; i < self.listItems().length; i++) {
-            self.listItems()[i].item.marker.setAnimation(null);
+        for (var i = 0; i < self.locationList().length; i++) {
+            self.locationList()[i].marker.setAnimation(null);
         }
     };
 
     var numResponse = 0;
-    var numResponseExpected = self.listItems().length;
-    for (var i = 0; i < self.listItems().length; i++) {
-        self.getMarker(self.listItems()[i].item.name, self.listItems()[i].item.address, numResponseExpected);
-        self.getThirdPartyData(self.listItems()[i].item.name, self.listItems()[i].item.address);
+    var numResponseExpected = self.locationList().length;
+    for (var i = 0; i < self.locationList().length; i++) {
+        self.getMarker(self.locationList()[i].name, self.locationList()[i].address, numResponseExpected);
+        self.getThirdPartyData(self.locationList()[i].name, self.locationList()[i].address);
     }
 
-    self.errorPrompt = function (error) {
-        alert(error);
-    };
+
 }
 
 function findItemInArray(array, query) {
     for (var i = 0; i < array().length; i++) {
-        if (array()[i].item.name === query) {
+        if (array()[i].name === query) {
             return i;
         }
     }
     return -1;
 }
 
-ko.applyBindings(new LocationViewModel());
